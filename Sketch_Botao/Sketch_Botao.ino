52908024fb01dd3352ebd59f8b161ce84acceb63
Vinicius Rocha <87396358+V1nnye@users.noreply.github.com>
2022-02-24 14:37:35 -0300

    Update Sketch_Botao.ino

--- Sketch_Botao/Sketch_Botao.ino
+++ Sketch_Botao/Sketch_Botao.ino
@@ -3,15 +3,15 @@ int LED = 12;
 int BOTAO = 9;
 
 void setup(){
-  pinMode(BOTAO, INPUT); // configura o pino com o botão como entrada
-  pinMode(LED, OUTPUT); // configura o pino com o LED como saída
+  pinMode(BOTAO, INPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop(){
-  if (digitalRead(BOTAO) == HIGH){ // se botão estiver pressionado (HIGH)
-    digitalWrite(LED, HIGH); // acende o LED
+  if (digitalRead(BOTAO) == HIGH){
+    digitalWrite(LED, HIGH);
   }
-  else{ // se não estiver pressionado (LOW)
-    digitalWrite(LED, LOW); // apaga o LED
+  else{
+    digitalWrite(LED, LOW);
   }
 }
